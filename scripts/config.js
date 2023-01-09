dccb51c7ef4ebbb884a146eecf1368df7b091b63
Pedro Cunha <pedrorfcunha@outlook.com>
2023-01-09 17:09:04 -0500

    concluded the configuration and switch players function, missing to check the winner

---
+++ scripts/config.js
@@ -0,0 +1,33 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;  /// +"1" = 1 (number, not string)
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim(); // Trim >>> "    Pedro Cunha   " => "Pedro Cunha"
+
+  if (!enteredPlayerName) {
+    // enteredPlayerName === ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById("player-" + editedPlayer + "-data");
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
+}
